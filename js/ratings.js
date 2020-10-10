7f5a6855901a8c068fd992accfde429064ff5787
kiminzajnr <kiminzaeric@gmail.com>
2020-10-10 17:01:59 +0300

    calculate average ratings

--- js/ratings.js
+++ js/ratings.js
@@ -3,7 +3,7 @@ collect_ratings=()=>{
   let rating=0;
   const elements=document.querySelectorAll(".rating");
 
-  elements.forEach(element, ()  => {
+  elements.forEach(element => {
     rating=parseInt(element.id.replace('star', ''));
     ratings.count+=parseInt(element.value);
     ratings.sum+=parseInt(element.value)*rating;
